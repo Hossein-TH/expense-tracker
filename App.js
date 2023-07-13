c1741155b01ce3da612de0baf6d2f08561b92a01
Hossein-TH <hossein1233897@gmail.com>
2023-07-13 17:30:13 +0330

    Created Folder Structure and added react navigation

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';;
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AllExpensesScreen from "./Screens/AllExpensesScreen";
+import RecentExpensesScreen from "./Screens/RecentExpensesScreen";
+import ManageExpenseScreen from "./Screens/ManageExpenseScreen";
+
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+
+const ExpenseOverview = () => {
+  return (
+      <BottomTabs.Navigator>
+          <BottomTabs.Screen name={'Recent Expenses'} component={RecentExpensesScreen}/>
+          <BottomTabs.Screen name={'All Expenses'} component={AllExpensesScreen}/>
+      </BottomTabs.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Fucking World!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name={'ExpensesOverview'} component={ExpenseOverview} />
+              <Stack.Screen name={'ManageExpense'} component={ManageExpenseScreen} />
+          </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
