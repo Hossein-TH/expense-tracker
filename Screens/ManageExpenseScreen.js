1cf6d9c89cb3afe028102a3758cdfbf0a5038388
Hossein-TH <hossein1233897@gmail.com>
2023-07-26 13:18:54 +0330

    Added Button Component and Used in ManageExpense

--- Screens/ManageExpenseScreen.js
+++ Screens/ManageExpenseScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, View } from "react-native";
 import { GlobalStyles } from "../Constants/Styles";
 import { useLayoutEffect } from "react";
 import IconButton from "../Components/UI/IconButton";
+import Button from "../Components/UI/Button";
 
 function ManageExpenseScreen({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -12,22 +13,32 @@ function ManageExpenseScreen({ route, navigation }) {
       title: isEditing ? "Edit Expense" : "Add Expense",
     });
   }, [navigation, isEditing]);
-  
-  const deleteExpenseHandler = () => {
-    
-  }
+
+  const deleteExpenseHandler = () => {};
+
+  const canselHandler = () => {};
+
+  const confirmHandler = () => {};
 
   return (
     <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} mode="flat" onPress={canselHandler}>
+          Cansel
+        </Button>
+        <Button style={styles.button} onPress={confirmHandler}>
+          {isEditing ? "Update" : "Add"}
+        </Button>
+      </View>
       {isEditing && (
-          <View style={styles.deleteContainer}>
-            <IconButton
-                icon="trash"
-                color={GlobalStyles.colors.error500}
-                size={36}
-                onPress={deleteExpenseHandler}
-            />
-          </View>
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
       )}
     </View>
   );
@@ -37,15 +48,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
-    backgroundColor: GlobalStyles.colors.primary800
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
     padding: 8,
     borderTopWidth: 2,
     borderTopColor: GlobalStyles.colors.primary200,
-    alignItems: 'center'
-  }
+    alignItems: "center",
+  },
 });
 
 export default ManageExpenseScreen;
