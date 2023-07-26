2d1bd3f6c38db635f472090a5a533021cd35a9f2
Hossein-TH <hossein1233897@gmail.com>
2023-07-26 12:53:10 +0330

    Added Delte IconButton to ManageExpense

--- Screens/ManageExpenseScreen.js
+++ Screens/ManageExpenseScreen.js
@@ -1,16 +1,51 @@
-import { Text } from 'react-native';
-import {useLayoutEffect} from "react";
-function ManageExpenseScreen({route, navigation}) {
-    const editedExpenseId = route.params?.expenseId;
-    const isEditing = !!editedExpenseId;
+import { StyleSheet, View } from "react-native";
+import { GlobalStyles } from "../Constants/Styles";
+import { useLayoutEffect } from "react";
+import IconButton from "../Components/UI/IconButton";
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            title: isEditing ? 'Edit Expense' : 'Add Expense'
-        });
-    },[navigation,isEditing])
+function ManageExpenseScreen({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
 
-    return <Text>ManageExpense Screen</Text>;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+  
+  const deleteExpenseHandler = () => {
+    
+  }
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+          <View style={styles.deleteContainer}>
+            <IconButton
+                icon="trash"
+                color={GlobalStyles.colors.error500}
+                size={36}
+                onPress={deleteExpenseHandler}
+            />
+          </View>
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+  deleteContainer: {
+    marginTop: 16,
+    padding: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  }
+});
 
 export default ManageExpenseScreen;
