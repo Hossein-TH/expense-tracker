a76c2e5f84a1176817dbb149fa1e26e2a8b74283
Hossein-TH <hossein1233897@gmail.com>
2023-08-03 13:19:26 +0330

    Added Validation to Form and Added Alert for Feedback

--- Components/ManageExpense/ExpenseForm.js
+++ Components/ManageExpense/ExpenseForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import Input from "../UI/Input";
 import Button from "../UI/Button";
 import { getFormattedDate } from "../../Util/Date";
@@ -19,15 +19,23 @@ function ExpenseForm(props) {
       };
     });
   };
-
   const submitHandler = () => {
-      const expenseData = {
-          amount: +inputValues.amount,
-          date: new Date(inputValues.date),
-          description: inputValues.description,
-      };
+    const expenseData = {
+      amount: +inputValues.amount,
+      date: new Date(inputValues.date),
+      description: inputValues.description,
+    };
 
-      onSubmit(expenseData);
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      Alert.alert("Invalid input", "Please check your input values!");
+      return;
+    }
+
+    onSubmit(expenseData);
   };
 
   return (
